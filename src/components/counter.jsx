ec8b7f4107e327959d56da2170817d26e5f9fa25
yunusparvezkhan <yunusparvezkhan@users.noreply.github.com>
2020-12-14 22:58:30 +0530

    Added Increment Feature

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,26 +2,40 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count : 0
+        count : 0, 
+        tags : ['tag1', 'tag2', 'tag3'],
+    };
+
+
+    handleIncrement = () => {
+        this.setState({ count : this.state.count + 1 });
     };
 
     render() {
         return (
         <div>
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm">Increment</button>
+            <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+            <ul>
+              {this.renderTags()}
+            </ul>
         </div>
         );
     }
 
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no tags</p> ; 
+        return this.state.tags.map(tag => <li key={tag}>{ tag }</li>)
+    }
+
     getBadgeClasses() {
-        let classes = "badge badge-m2 badge-";
+        let classes = "badge m-2 badge-";
         classes += this.state.count === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const { count } = this.state;
+        let { count } = this.state;
         return count === 0 ? "Zero" : count;
     }
 
