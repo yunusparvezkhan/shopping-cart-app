5a5f57f9f14e79a95356b59eabb868837e999904
yunusparvezkhan <yunusparvezkhan@users.noreply.github.com>
2020-12-14 13:21:50 +0530

    Created Status Bar

---
+++ src/components/counter.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        count : 0
+    };
+
+    render() {
+        return (
+        <div>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
+        </div>
+        );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge badge-m2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? "Zero" : count;
+    }
+
+}
+
+ 
+export default Counter;
