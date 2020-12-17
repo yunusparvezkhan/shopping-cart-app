ee68abd8a02717da9e6da280286e44f78c020ff7
yunusparvezkhan <yunusparvezkhan@users.noreply.github.com>
2020-12-17 16:48:08 +0530

    Learnt about passing children

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count : 0, 
+        value : this.props.value, 
         tags : ['tag1', 'tag2', 'tag3'],
     };
 
 
     handleIncrement = () => {
-        this.setState({ count : this.state.count + 1 });
+        this.setState({ value : this.state.value + 1 });
     };
 
     render() {
         return (
         <div>
+            <h6>{ this.props.id }</h6>
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
             <ul>
@@ -30,13 +31,13 @@ class Counter extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        let { count } = this.state;
-        return count === 0 ? "Zero" : count;
+        let { value } = this.state;
+        return value === 0 ? "Zero" : value;
     }
 
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,16 +4,21 @@ import Counter from './counter'
 class Counters extends Component {
     state = { 
         counters : [
-            { id : 1 , value : 0 },
-            { id : 2 , value : 0 },
-            { id : 3 , value : 0 },
-            { id : 4 , value : 0 }
-        ]
+            { id : 0 , value : 0 , title : "Yunus" },
+            { id : 1 , value : 1 , title : "Ritwik" },
+            { id : 2 , value : 2 , title : "Sahil" },
+            { id : 3 , value : 3 , title : "Ishu" },
+            { id : 4 , value : 4 , title : "Supriyo" },
+            { id : 5 , value : 5 , title : "Tannu" },
+            { id : 6 , value : 6 , title : "Manish" }
+        ],
      }
     render() { 
         return ( 
             <div>
-                { this.state.counters.map(counters => <Counter key={counters.key}/>)}
+                { this.state.counters.map(counter => 
+                    <Counter key={counter.id} value={counter.value} id={counter.title} />
+                )}
             </div>
          );
     }
